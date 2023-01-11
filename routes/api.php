85524264894caede0b2e329a601bfd5e9027949b
winimk <winimuthia7@gmail.com>
2023-01-11 08:34:46 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,14 @@ Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'regis
 //API route for login user
 Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
 
+Route::get('/get_by_status_paket/{id}', [App\Http\Controllers\API\PaketController::class, 'get_by_status']);
+
 //Protecting Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
 
     Route::get('/get_all_paket', [App\Http\Controllers\API\PaketController::class, 'index']);
     Route::get('/get_by_id_paket/{id}', [App\Http\Controllers\API\PaketController::class, 'get_by_id']);
-    Route::get('/get_by_status_paket/{id}', [App\Http\Controllers\API\PaketController::class, 'get_by_status']);
     Route::post('/create_paket', [App\Http\Controllers\API\PaketController::class, 'create']);
     Route::post('/update_paket', [App\Http\Controllers\API\PaketController::class, 'update']);
     Route::get('/delete_paket/{id}', [App\Http\Controllers\API\PaketController::class, 'destroy']);
